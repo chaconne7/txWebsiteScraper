410acb4fc4875f2aec7dca11c0f5167278d6cb4f
chaconne7 <whitekeyblackkey@comcast.net>
2016-08-01 13:13:40 -0700

    Scrapes counties.

--- scraper.rb
+++ scraper.rb
@@ -3,15 +3,24 @@ require 'Nokogiri'
 require 'open-uri'
 require 'csv'
 
+
 city_html = Nokogiri::HTML(open('http://www.tml.org/links_cities'))
 
+county_html = Nokogiri::HTML(open('https://www.county.org/about-texas-counties/county-websites/Pages/default.aspx'))
+
+
 cities = city_html.css('p').css('a')
+counties = county_html.css('.underlinelink')
+
 CSV.open("txIssuerWebsites.csv", "wb") do |csv|
   csv << ["type", "name", "link"]
+  
   cities.each do |city|
-    puts city.text
-    puts city['href']
     csv << ["city", city.text, city['href']]
+  end
+
+  counties.each do |county|
+    csv << ["county", county.text, county['href']]
   end
 end
 
