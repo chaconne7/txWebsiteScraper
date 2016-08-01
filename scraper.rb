fd78f41af5392d1aa3db0d162a330258186546fa
chaconne7 <whitekeyblackkey@comcast.net>
2016-08-01 12:58:20 -0700

    Scrapes city websites.

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,18 @@
 require 'rubygems'
-require 'nokogiri'
+require 'Nokogiri'
 require 'open-uri'
+require 'csv'
 
 city_html = Nokogiri::HTML(open('http://www.tml.org/links_cities'))
-puts city_html.class
+
+cities = city_html.css('p').css('a')
+CSV.open("txIssuerWebsites.csv", "wb") do |csv|
+  csv << ["type", "name", "link"]
+  cities.each do |city|
+    puts city.text
+    puts city['href']
+    csv << ["city", city.text, city['href']]
+  end
+end
+
+
